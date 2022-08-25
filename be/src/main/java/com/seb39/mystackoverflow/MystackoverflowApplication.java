d8c7a112c5258775799aafb1efa693399a40db96
o01071767279@nate.com <o01071767279@nate.com>
2022-08-25 12:02:26 +0900

    initial commit

---
+++ be/src/main/java/com/seb39/mystackoverflow/MystackoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.seb39.mystackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystackoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystackoverflowApplication.class, args);
+	}
+
+}
