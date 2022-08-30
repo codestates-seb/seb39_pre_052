fd9efac1d521686da091bf5ad8375159ebe4f834
imsujinpark <imsujinpark@gmail.com>
2022-08-30 18:24:59 +0900

    new text editor added using Quill.js

--- fe/src/components/Editor.js
+++ fe/src/components/Editor.js
@@ -1,34 +1,58 @@
+import { useState } from "react";
 import styled from "styled-components"
+// import ReactQuill from 'react-quill';
+// import 'react-quill/dist/quill.snow.css';
+import Toolbox from "./Toolbox";
 
 const Container = styled.div`
     flex-basis: 100vw;
+    width: 65vw;
     flex-shrink: 6;
+    display: flex;
+    flex-direction: column;
+    padding: 10px 50px 0 50px;
     > * {
         margin-top: 30px;
     }
     // Title & Body
     > * > h1 {
-
+        font-size: 18px;
+    }
+    // p tag for Title and Body
+    > * > p {
+        font-size: 14px;
+    }
+    > * > * {
+        margin-bottom: 5px;
     }
 `
 
 const Header = styled.div`
-
+    > input {
+        width: 100%;
+        height: 34px;
+        border: 1px solid #D1D1D1;
+        padding-left: 10px; 
+    }
 `
 
 const Suggestion = styled.div`
+    display: none;
+    border: 1px solid black;
 
 `
 
 const Body = styled.div`
-
+    /* > textarea {
+        width: 100%;
+        height: 30vh;
+    } */
 `
 
 const Button = styled.button`
     background-color: #0A95FF;
     border: none;
     padding: 15px;
-    margin-right: 15px;
     color: white;
     font-size: 16px;
     font-weight: bold;
@@ -38,12 +62,14 @@ const Button = styled.button`
 `
 
 const Editor = () => {
+    const [htmlStr, setHtmlStr] = useState('');
+
     return (
         <Container>
             <Header>
                 <h1>Title</h1>
                 <p>Be specific and imagine you’re asking a question to another person</p>
-                <input type="text"></input>
+                <input type="text" placeholder="e.g. Is there an R function for finding the index of an element in a vector?"></input>
             </Header>
             <Suggestion>
                 <div>Similar questions</div>
@@ -52,9 +78,11 @@ const Editor = () => {
             <Body>
                 <h1>Body</h1>
                 <p>Include all the information someone would need to answer your question</p>
-                <textarea></textarea>
+                {/* <ReactQuill theme="snow" value={value} onChange={setValue} /> */}
+                <Toolbox htmlStr={htmlStr} setHtmlStr={setHtmlStr}></Toolbox>
             </Body>
             <Button>Submit</Button>
+            {/* {htmlStr} // google how to interpret text as jsx */}
         </Container>
     )
 };
--- fe/src/pages/NewQuestion.js
+++ fe/src/pages/NewQuestion.js
@@ -1,10 +1,27 @@
+import styled from "styled-components";
 import Editor from "../components/Editor";
 
+const Container = styled.div`
+    flex-basis: 100vw; 
+    flex-shrink: 6;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+`
+
+const Header = styled.div`
+    padding: 50px 0 0 50px;
+    font-size: 28px;
+    font-weight: bold;
+`
 const NewQuestion = () => {
     return (
-        <>
+        <Container>
+            <Header>
+                Ask a public question
+            </Header>
             <Editor />
-        </>
+        </Container>
     )
 };
 
