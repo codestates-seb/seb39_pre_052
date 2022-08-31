bcf2a1cbdc314dc7d6288c8e06e5254687eada81
imsujinpark <imsujinpark@gmail.com>
2022-08-31 23:53:14 +0900

    Questions page now render main data with correct pagination

--- fe/src/pages/Questions.js
+++ fe/src/pages/Questions.js
@@ -4,6 +4,65 @@ import styled from "styled-components";
 import Question from '../components/Question';
 import Pagination from '../components/Pagination';
 
+const Questions = () => {
+    const [posts, setPosts] = useState([]);
+    const [limit, setLimit] = useState(5);
+    const [page, setPage] = useState(1);
+    const offset = (page - 1) * limit;
+
+    // // TEST DATA
+    // useEffect(() => {
+    //     fetch(`/test/question?size=${limit}&page=${page}`)
+    //         .then((res) => res.json())
+    //         .then((data) => setPosts(data.data))
+    //         .catch((err) => console.log(`!CANNOT FETCH QUESTION DATA! ${err}!`))
+    // }, [page, limit]);
+    
+    useEffect(() => {
+        fetch(`/api/questions?size=${limit}&page=${page}`)
+            .then((res) => res.json())
+            .then((data) => setPosts(data.data))
+            .catch((err) => console.log(`!CANNOT FETCH QUESTION DATA! ${err}!`))
+    }, [page, limit]);
+
+    return (
+        <Container>
+            <Header>
+                <div>
+                    <div>All Questions</div>
+                    <div>{posts.length} questions</div>
+                </div>
+                <div>
+                    <Link to="/questions/ask">
+                        <Button>Ask Question</Button>
+                    </Link>
+                </div>
+            </Header>
+            {/* The below is for TEST DATA, slicing data from client side
+            <List>
+                {posts.slice(offset, offset + limit).map((post, idx) => {
+                    return <Question key={idx} post={post}></Question>
+                })}
+            </List> */}
+            {/* The below is for MAIN DATA, server side will send sliced data*/}
+            <List>
+                {posts.map((post, idx) => {
+                    return <Question key={idx} post={post}></Question>
+                })}
+            </List>
+            <Pagination
+                total={posts.length}
+                limit={limit}
+                page={page}
+                setPage={setPage}
+                setLimit={setLimit}
+            />
+        </Container>
+    )
+};
+
+// Styled Components
+
 const Container = styled.div`
     flex-basis: 100vw; 
     flex-shrink: 6;
@@ -46,57 +105,6 @@ const List = styled.div`
     overflow-y: scroll;
     max-height: 75vh;
 `
-
-const Questions = () => {
-    const [posts, setPosts] = useState([]);
-    const [limit, setLimit] = useState(5);
-    const [page, setPage] = useState(1);
-    const offset = (page - 1) * limit;
-
-
-    useEffect(() => {
-        fetch(`/test/question?size=${limit}&page=${page}`)
-            .then((res) => res.json())
-            .then((data) => setPosts(data.data))
-            .catch((err) => console.log(`!CANNOT FETCH QUESTION DATA! ${err}!`))
-    }, [page, limit]);
-    
-
-    return (
-        <Container>
-            <Header>
-                <div>
-                    <div>All Questions</div>
-                    <div>{posts.length} questions</div>
-                </div>
-                <div>
-                    <Link to="/questions/ask">
-                        <Button>Ask Question</Button>
-                    </Link>
-                </div>
-            </Header>
-            {/* The below is for TEST DATA, slicing data from client side*/}
-            <List>
-                {posts.slice(offset, offset + limit).map((post, idx) => {
-                    return <Question key={idx} post={post}></Question>
-                })}
-            </List>
-            {/* The below is for MAIN DATA, server side will send sliced data*/}
-            {/* <List>
-                {posts.map((post, idx) => {
-                    return <Question key={idx} post={post}></Question>
-                })}
-            </List> */}
-            <Pagination
-                total={posts.length}
-                limit={limit}
-                page={page}
-                setPage={setPage}
-                setLimit={setLimit}
-            />
-        </Container>
-    )
-};
 
 export default Questions;
 
