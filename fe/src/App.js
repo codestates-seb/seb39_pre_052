b9c00d2f66b5351854e082b42642e843ecc53023
imsujinpark <imsujinpark@gmail.com>
2022-08-31 23:48:09 +0900

    new question data can be posted to server & verification test added for title and content input

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import GlobalStyle from "./GlobalStyle";
 
 import ReduxTest from "./components/ReduxTest";
-
 import GNB from "./components/GNB";
 import SNB from "./components/SNB";
 import Questions from "./pages/Questions";
--- fe/src/components/Editor.js
+++ fe/src/components/Editor.js
@@ -1,11 +1,130 @@
-import { useState } from "react";
 import styled from "styled-components"
+import { useState, useRef } from "react";
+import { useNavigate } from 'react-router-dom';
 import { Markup } from 'interweave'; // react library to interpret html string to jsx
 
-// import ReactQuill from 'react-quill';
-// import 'react-quill/dist/quill.snow.css';
 import Toolbox from "./Toolbox";
 
+const Editor = () => {
+    const [title, setTitle] = useState(""); //Title
+    const [htmlStr, setHtmlStr] = useState(""); //Body (html string)
+    const [isTitleEmpty, setIsTitleEmpty] = useState(true);
+    const [isContentEmpty, setIsContentEmpty] = useState(true);
+    const [emptyTitleMsg, setEmptyTitleMsg] = useState("");
+    const [emptyContentMsg, setEmptyContentMsg] = useState("");
+
+    const navigate = useNavigate();
+
+    const titleRef = useRef();
+    const contentRef = useRef();
+
+    const token = "Beare eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsb2dpbiBqd3QgdG9rZW4iLCJleHAiOjE2NjE5Mzg0MTIsImVtYWlsIjoic3VqaW5AZ21haWwuY29tIn0.IJSdhLrrTLCHErux_jnPbIfthF3D9rjsaIGBgpgPuFA9FyQ0GcqwwcCx90PNaAlWkFsg8KmmTcCE3G9APXLwhA";
+
+    const handleTitleInput = (e) => {
+        setTitle(e.target.value);
+        console.log(`here: ${title}`)
+        
+        // The red essage disappears when input is entered
+        if (title.length < 0) {
+            setIsTitleEmpty(true);
+            setEmptyTitleMsg("Title is missing.");
+        }
+        else {
+            setIsTitleEmpty(false);
+            setEmptyTitleMsg("");
+        }
+    }
+
+    const handleSubmit = () => {
+
+        // 유효성 검사
+        if (title.length === 0) {
+            // 제목, 컨텐츠 둘다 비었을 때
+            if (htmlStr === '<p><br></p>' || htmlStr === '<p></p>' || htmlStr.length === 0) {
+                titleRef.current.focus();
+                setEmptyTitleMsg("Title is missing.");
+                setEmptyContentMsg("Body is missing.");
+            }
+            // 제목만 비었을 때
+            else {
+                titleRef.current.focus();
+                setEmptyTitleMsg("Title is missing.");
+                setEmptyContentMsg("");
+            }
+        }
+        // 컨텐츠만 비었을 때
+        else if (htmlStr === '<p><br></p>' || htmlStr === '<p></p>' || htmlStr.length === 0) {
+            contentRef.current.focus();
+            setEmptyContentMsg("Body is missing.")
+            setEmptyTitleMsg("");
+            console.log(htmlStr);
+        }
+        // 유효성 검사 통과
+        else {
+            fetch("/api/questions", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json, text/plain',
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    "Authorization": token,
+                },
+                body: JSON.stringify({ title: title, content: htmlStr }),
+            })
+            .then((res) => {
+                if (res.status === 201) {
+                    console.log(res);
+                    alert(`Successfully Submitted!`)
+                    navigate("/"); // 질문 상세 페이지로 변경 예정
+                }
+                // else if to be deleted
+                else if (res.status === 500) {
+                    console.log(`ERROR: check your token`)
+                }
+                else {
+                    console.log(res);
+                    alert(`ERROR: ${res.status}`)
+                }
+            });
+        }
+    }
+
+    return (
+        <Container>
+            <Header>
+                <h1>Title</h1>
+                <p>Be specific and imagine you’re asking a question to another person</p>
+                <input 
+                    type="text" 
+                    placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
+                    onChange={handleTitleInput}
+                    ref={titleRef}
+                ></input>
+                <Msg>{emptyTitleMsg}</Msg>
+            </Header>
+            <Suggestion>
+                <div>Similar questions</div>
+                <div>content</div>
+            </Suggestion>
+            <Body>
+                <h1>Body</h1>
+                <p>Include all the information someone would need to answer your question</p>
+                {/* <ReactQuill theme="snow" value={value} onChange={setValue} /> */}
+                <Toolbox
+                    htmlStr={htmlStr}
+                    setHtmlStr={setHtmlStr}
+                    contentRef={contentRef}
+                    setIsContentEmpty={setIsContentEmpty}
+                    setEmptyContentMsg={setEmptyContentMsg}
+                ></Toolbox>
+                <Msg>{emptyContentMsg}</Msg>
+            </Body>
+            <Button onClick={handleSubmit}>Submit</Button>
+            <Markup content={htmlStr} />
+        </Container>
+    )
+};
+
+// Styled Components
 const Container = styled.div`
     flex-basis: 100vw;
     width: 65vw;
@@ -51,6 +170,10 @@ const Body = styled.div`
     } */
 `
 
+const Msg = styled.div`
+    color: red;
+`
+
 const Button = styled.button`
     background-color: #0A95FF;
     border: none;
@@ -62,32 +185,6 @@ const Button = styled.button`
         background-color: #0074CC;
     }
 `
-
-const Editor = () => {
-    const [htmlStr, setHtmlStr] = useState('');
-
-    return (
-        <Container>
-            <Header>
-                <h1>Title</h1>
-                <p>Be specific and imagine you’re asking a question to another person</p>
-                <input type="text" placeholder="e.g. Is there an R function for finding the index of an element in a vector?"></input>
-            </Header>
-            <Suggestion>
-                <div>Similar questions</div>
-                <div>content</div>
-            </Suggestion>
-            <Body>
-                <h1>Body</h1>
-                <p>Include all the information someone would need to answer your question</p>
-                {/* <ReactQuill theme="snow" value={value} onChange={setValue} /> */}
-                <Toolbox htmlStr={htmlStr} setHtmlStr={setHtmlStr}></Toolbox>
-            </Body>
-            <Button>Submit</Button>
-            <Markup content={htmlStr} />
-        </Container>
-    )
-};
 
 export default Editor;
 
