dedde8aa02193c5e3a4cb729da4decccc6bd6888
sj <slaayer60@gmail.com>
2022-08-31 14:22:14 +0900

    temp

--- be/src/test/java/com/seb39/mystackoverflow/controller/AnswerControllerTest.java
+++ be/src/test/java/com/seb39/mystackoverflow/controller/AnswerControllerTest.java
@@ -20,12 +20,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -54,15 +66,16 @@ class AnswerControllerTest {
     MemberRepository memberRepository;
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         Member member = Member.builder()
                 .email("abcd@naver.com")
                 .roles("ROLE_USER")
                 .build();
         memberRepository.save(member);
     }
+
     @AfterEach
-    void afterEach(){
+    void afterEach() {
         memberRepository.deleteAll();
     }
 
@@ -81,11 +94,21 @@ class AnswerControllerTest {
         String requestJson = objectMapper.writeValueAsString(requestDto);
 
         // expected
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/answers")
+        mockMvc.perform(post("/api/answers")
                         .param("question-id", "1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("answer-create",
+                        requestParameters(
+                                parameterWithName("question-id").description("작성한 답변의 질문 ID")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("답변 내용")
+                                )
+                        )
+                ));
     }
 
     @Test
@@ -94,27 +117,42 @@ class AnswerControllerTest {
         AnswerDto.Patch requestDto = new AnswerDto.Patch();
         requestDto.setContent("updated Answer");
 
-        BDDMockito.given(answerMapper.answerPatchToAnswer(any(),any()))
+        BDDMockito.given(answerMapper.answerPatchToAnswer(any(), any()))
                 .willReturn(new Answer());
 
-        BDDMockito.doNothing().when(answerService).updateAnswer(any(),any());
+        BDDMockito.doNothing().when(answerService).updateAnswer(any(), any());
 
         String requestJson = objectMapper.writeValueAsString(requestDto);
 
         // expected
-        mockMvc.perform(MockMvcRequestBuilders.patch("/api/answers/1")
+        mockMvc.perform(patch("/api/answers/{answerId}",1)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("answer-update",
+                        pathParameters(
+                                parameterWithName("answerId").description("수정할 답변의 ID")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("답변 내용")
+                                )
+                        )
+                ));
     }
 
     @Test
     void deleteAnswerTest() throws Exception {
         // given
-        BDDMockito.doNothing().when(answerService).deleteAnswer(any(),any());
+        BDDMockito.doNothing().when(answerService).deleteAnswer(any(), any());
 
         // expected
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/answers/1"))
-                .andExpect(status().isOk());
+        mockMvc.perform(delete("/api/answers/{answerId}",1))
+                .andExpect(status().isOk())
+                .andDo(document("answer-delete",
+                        pathParameters(
+                                parameterWithName("answerId").description("수정할 답변의 ID")
+                        )
+                ));
     }
 }
