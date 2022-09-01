8fcae08bde9efbfc31719d3cc5a2158349192d3b
o01071767279@nate.com <o01071767279@nate.com>
2022-09-01 09:47:05 +0900

    작성자별 질문 조회 기능 수정

--- be/src/main/java/com/seb39/mystackoverflow/service/QuestionService.java
+++ be/src/main/java/com/seb39/mystackoverflow/service/QuestionService.java
@@ -62,8 +62,8 @@ public class QuestionService {
     }
 
     //5. 회원 질문 조회
-    public Page<Question> findQuestions(Long memberId, int page, int size) {
-        return questionRepository.findAllByMemberId(memberId, PageRequest.of(page, size, Sort.by("id").descending()));
+    public Page<Question> findQuestions(Long memberId, int page) {
+        return questionRepository.findAllByMemberId(memberId, PageRequest.of(page, 30, Sort.by("id").descending()));
     }
 
     //해당 질문이 존재하는지 확인하는 메서드
