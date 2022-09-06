beb5847e3114c9aed90d69c52ac15988c71463a8
imsujinpark <imsujinpark@gmail.com>
2022-09-06 14:04:27 +0900

    minor changes

--- fe/src/index.js
+++ fe/src/index.js
@@ -11,7 +11,7 @@ import store from "./store"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // disable all console.log
-// console.log = function() {}
+console.log = function() {}
 
 root.render(
   <React.StrictMode>
