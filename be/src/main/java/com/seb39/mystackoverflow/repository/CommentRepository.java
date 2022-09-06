8548faf827eb8dc0668c4a6f321bdfcbbe153162
sj <slaayer60@gmail.com>
2022-09-06 14:43:04 +0900

    remove be

--- be/src/main/java/com/seb39/mystackoverflow/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seb39.mystackoverflow.repository;
-
-import com.seb39.mystackoverflow.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
